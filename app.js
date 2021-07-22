667bbf1f936e12a62a3ff2648bfd2bf3785f9602
xiaohua2003 <xcai3@ualberta.ca>
2021-07-22 16:44:41 -0600

    update app.js,css

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 document.getElementById("loan-form").addEventListener("submit", calculateLoan);
 function calculateLoan(e){
+    //UI
     const amount = document.querySelector('#amount').value;
     const interest_rate = document.querySelector('#interest').value;
-    const months = document.querySelector('#months').value;
-    const interest = (amount * (interest_rate * 0.01)) / months;
-    const payment = ((amount / months) + interest).toFixed(2);
-    document.querySelector('#payment').innerHTML =  'Monthly payment ($):' + payment;
+    const years = document.querySelector('#years').value;
+    //Calculate
+    const principal=parseFloat(amount);
+    const CalculateInterest=parseFloat(interest_rate)/100/12;
+    const CalculatedPayments=parseFloat(years)*12;
+    //calculate montly payment
+    const x=Math.pow(1+CalculateInterest, CalculatedPayments);
+    const montly=(principal*x*CalculateInterest)/(x-1);
+    const payment=montly.toFixed(2);
+    //calculate Interest
+    const totalInterest=(montly*CalculatedPayments-principal).toFixed(2);
+    //calculate total payments
+    const totalPayment=(montly*CalculatedPayments).toFixed(2);
+    document.querySelector('#payment').innerHTML=payment;
+    document.querySelector("#totalInterest").innerHTML=totalInterest;
+    document.querySelector("#totalPayment").innerHTML=totalPayment;
     e.preventDefault();
 }
